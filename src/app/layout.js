c848347f60da987454d6e9b8b266600c8e1d1309
alaaeallam <alaaeallam@gmail.com>
2025-07-29 22:41:09 +0300

    remove Codegrid

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,8 @@ import Nav from "./components/Nav/Nav";
 import { ViewTransitions } from "next-view-transitions";
 
 export const metadata = {
-  title: "Rania Maged | MWT by Codegrid",
-  description: "MWT by Codegrid",
+  title: "Rania Maged",
+  description: "Rania Maged",
 };
 
 export default function RootLayout({ children }) {
